9bc28b6994ee5016b316bcc8ca50ad8b7dd17b88
Luke Roberts <luke@sqwiggle.com>
2015-07-27 11:31:39 +0200

    Basic rabbit implementation

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"github.com/streadway/amqp"
 	"log"
+)
+
+var (
+	rabbitConn    *amqp.Connection
+	rabbitChannel *amqp.Channel
 )
 
 func main() {
 	log.Print("Croft is ALIVE")
+
+	var err error
+	rabbitConn, err = EnsureRabbitConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rabbitConn.Close()
+	rabbitChannel, err = ConfigureRabbit()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = PublishRabbitMessage()
+	if err != nil {
+		log.Fatal(err)
+	}
 	go StartUDPServer(1700)
 	ServeHTTPOverview()
 }
